d56f80e8c65e5702223b92bb2ee34c900d84c486
Juan Hoyos <juanhoyos1347@gmail.com>
2023-10-20 23:23:58 -0500

    models

---
+++ database/models/UserPrestamo.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const db = require('../connection');
+const User = require('./User');
+const Libro = require('./Libro');
+const UserPrestamo = db.define('usuario_prestamo',{
+    id_prestamo:{
+        type:DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey:true
+    },
+    fecha_prestamo: {
+        type: DataTypes.DATE
+    },
+    fecha_devolucion:{
+        type:DataTypes.DATE
+    },
+    devuelto:{
+        type:DataTypes.BOOLEAN
+    },
+    id_usuario: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id_usuario'
+        }
+    },
+    id_libro: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Libro,
+            key: 'id_libro'
+        }
+    },
+    timestamps: false,
+    freezeTableName: true,
+    tableName: 'usuario_prestamo'
+})
+
+
+UserPrestamo.belongsTo(User,{foreignKey: 'id_usuario'});
+UserPrestamo.belongsTo(Libro,{foreignKey: 'id_libro'});
+module.exports= User
