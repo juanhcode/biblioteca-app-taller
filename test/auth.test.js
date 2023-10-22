becca5e7d77c8b829d21184e75c83662473e0991
sebastian <sebastian.cifuentes0808@gmail.com>
2023-10-22 11:50:43 -0500

    Correccion en auth.test.js

--- test/auth.test.js
+++ test/auth.test.js
@@ -21,8 +21,8 @@ describe('Pruebas de inicio de sesión', () => {
                 contrasenia: "1234454"
             });
 
-        expect(response.status).toBe(401);
-        expect(response.body.msg).toBe('Credenciales incorrectas');
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe('No existe el usuario: usuarioNoExiste');
     });
     it('Debería fallar el inicio de sesión si el usuario no existe', async () => {
         const response = await request(app)
@@ -33,7 +33,7 @@ describe('Pruebas de inicio de sesión', () => {
             });
 
         expect(response.status).toBe(404);
-        expect(response.body).toHaveProperty('error', 'Usuario no encontrado');
+        expect(response.body.msg).toBe(`No existe el usuario: usuarioNoExiste`);
     });
     it('Debería fallar el inicio de sesión con contraseña incorrecta', async () => {
         const response = await request(app)
@@ -52,7 +52,7 @@ describe('Pruebas de inicio de sesión', () => {
             .send({});
 
         expect(response.status).toBe(400);
-        expect(response.body).toHaveProperty('error', 'Falta información de inicio de sesión');
+        expect(response.body.msg).toBe('Falta información de inicio de sesión');
     });
 
 })
